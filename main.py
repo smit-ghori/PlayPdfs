c85034f950673043f3855b7d0911bb216dbdf3f1
Smit-Ghori <smitghori83@gmail.com>
2026-03-06 22:45:24 +0530

    fix render port issue

--- main.py
+++ main.py
@@ -67,7 +67,3 @@ def merge():
 @app.route('/download/<folder_id>')
 def download(folder_id):
     return render_template("download.html")
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
